65ac8b7f83a195f0312076d3a35cc80ae11b5596
Nevin Quach <nquach3@gmu.edu>
2026-03-12 01:01:28 +0000

    Commit

---
+++ pratice.js
@@ -0,0 +1,15 @@
+// Basic information
+alert ("This JavaScript is for Pratice.HTML");
+console.log ("This JavaScript is for Pratice.HTML");
+
+// Displays the number 0
+let number = 0
+let finalnumber = number
+alert (finalnumber);
+console.log ("This is the number" + finalnumber);
+
+// Code for Pratice Button
+let testbutton = document.getElementById("testbutton");
+testbutton.addEventListener("click", function() {
+    
+});
