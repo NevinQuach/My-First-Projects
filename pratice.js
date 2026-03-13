193afa611429c6562936763a688155e344d7ca16
Nevin Quach <nquach3@gmu.edu>
2026-03-13 00:18:15 +0000

    Started building form

--- pratice.js
+++ pratice.js
@@ -1,15 +1,20 @@
 // Basic information
-alert ("This JavaScript is for Pratice.HTML");
+// alert ("This JavaScript is for Pratice.HTML");
 console.log ("This JavaScript is for Pratice.HTML");
 
 // Displays the number 0
 let number = 0
 let finalnumber = number
-alert (finalnumber);
-console.log ("This is the number" + finalnumber);
+// alert (finalnumber);
+console.log ("This is the number " + finalnumber);
 
 // Code for Pratice Button
 let testbutton = document.getElementById("testbutton");
+let numbermessage = document.getElementById("numbermessage");
+let nameinput = document.getElementById("nameinput");
+
 testbutton.addEventListener("click", function() {
-    
+    let username = nameinput.value;
+    numbermessage.textContent = "Hello " + username;
+    numbermessage.style.display = "block";
 });
